3f2bf0fcfe8dfb084cb9b64c0e052360b5ce1dba
antonyy217 <153318661+antonyy217@users.noreply.github.com>
2024-03-01 08:29:35 -0300

    Delete tell2 directory

--- tell2/telefone.js
+++
@@ -1,9 +0,0 @@
-function AddNum(number) {
-    var NumDigitado= document.getElementById('exibir');
-
-    NumDigitado.value += number;
-  }
-  
-function ApagarNum() {
-  document.getElementById('exibir').value = "" ;
-}
